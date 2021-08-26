7228569d06530125dbf08ec380030d29e84e3c6a
Norman Walsh <ndw@nwalsh.com>
2021-08-26 13:46:59 +0100

    Testing CI

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "$CIRCLE_TAG" ]; then
-    echo "Only tagged commits are published"
-    exit 0
-fi
+#if [ -z "$CIRCLE_TAG" ]; then
+#    echo "Only tagged commits are published"
+#    exit 0
+#fi
 
-./gradlew dotnetNugetPush
+./gradlew -PnugetApiKey=WRONG$NUGETAPIKEY -PnugetSource=$NUGETSOURCE helloWorld
